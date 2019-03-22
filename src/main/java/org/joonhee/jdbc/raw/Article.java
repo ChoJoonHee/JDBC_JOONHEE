a2d461152b2cafcff459563c3f2a4fad8357f3ff
joonhee <slaxer@hanmail.net>
2019-03-22 10:40:48 +0900

    JOONHEE

---
+++ src/main/java/org/joonhee/jdbc/raw/Article.java
@@ -0,0 +1,52 @@
+package org.joonhee.jdbc.raw;
+
+public class Article {
+	String articleId;
+	String title;
+	String content;
+	String userId;
+	String name;
+	String cdate;
+	public String getArticleId() {
+		return articleId;
+	}
+	@Override
+	public String toString() {
+		return "Article [articleId=" + articleId + ", title=" + title + ", content=" + content + ", userId=" + userId
+				+ ", name=" + name + ", cdate=" + cdate + "]";
+	}
+	public void setArticleId(String articleId) {
+		this.articleId = articleId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCdate() {
+		return cdate;
+	}
+	public void setCdate(String cate) {
+		this.cdate = cate;
+	}
+	
+}
